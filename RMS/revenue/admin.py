6460e75ca0e32d13fcb82ace34b4905f04e5a253
bhartikatre <74614660+bhartikatre@users.noreply.github.com>
2021-07-17 19:42:02 +0300

    added in model customers
    added customer class in model
    explored possibility if calculated fields commented code
    explored foreign key primary keey commented code

--- RMS/revenue/admin.py
+++ RMS/revenue/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Revenue,Customers
+from .models import Revenue,Customer
 
 # Register your models here
+#admin.site.register(Revenue,RevenueAdmin)
 admin.site.register(Revenue)
-admin.site.register(Customers)
+admin.site.register(Customer)
+
+#class RevenueAdmin(admin.ModelAdmin):
+ #   list_display = ['cust_name','cust_id','invoice_amount_euro','transaction_date','cost_of_goods_euro','account_invoice_received',
+ #                  'revenue_account_name','operational_costs_euro','revenue_charged_euro']
+    #readonly_fields = ('revenue_charged_euro')
--- RMS/revenue/models.py
+++ RMS/revenue/models.py
@@ -1,28 +1,34 @@
 from django.db import models
 
 # Create your models here.
-class Customers(models.Model):
-    customer_name   = models.CharField(max_length=50,null=False)
+class Customer(models.Model):
+    cust_name       = models.CharField(max_length=50,null=False,primary_key=True)
     cust_id         = models.PositiveIntegerField(unique=True)
     cust_address    = models.CharField(max_length=70,null=False)
 
     def __str__(self):
-        return self.customer_name
-        return self.cust_address
+        return self.cust_name
 
 class Revenue(models.Model):
-    cust_name                = models.CharField(max_length=50,null=False)
-    cust_id                 = models.PositiveIntegerField(unique=True)
+    cust_id                = models.ForeignKey('Customer',null=False,on_delete=models.PROTECT)
     invoice_amount_euro     = models.DecimalField(decimal_places=2,null=False,max_digits=6)
     transaction_date        = models.DateField(null=False)
     revenue_charged_euro    = models.DecimalField(default=0,decimal_places=2,max_digits=6)
     cost_of_goods_euro      = models.DecimalField(default=0,decimal_places=2,max_digits=6)
     account_invoice_received = models.PositiveIntegerField(null=False)
-    revenue_account_number  = models.PositiveIntegerField(null=False)
+    revenue_account_name  = models.CharField(max_length=50,null=False,default=111001)
     operational_costs_euro  = models.DecimalField(default=0,decimal_places=2,max_digits=6)
 
+   # @property
+   # def _get_revenue_charged_euro(self):
+   #         #Returns 10% of amount received as revenue (rest 90% is either services or goods delievered to the customer)
+   #         return  self.invoice_amount_euro * 0.1
+   # revenue_charged_euro = property(_get_revenue_charged_euro)
+
     def __str__(self):
-        return self.revenue_account_number
+        return self.revenue_account_name
+        return self.cust_id              #not returning chk with foreign key ask
+
 
 #class GM(models.Model):
  #   revenue_account_rollup  = models.DecimalField(decimal_places=2,null=False,max_digits=6)
